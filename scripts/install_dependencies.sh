25a6c3abccbf570056a453909d8f7f295986c917
kirihar2 <kirihara.juran@gmail.com>
2019-05-09 10:03:41 -0700

    upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
     
 #!/bin/bash
-if yum list installed | grep nodejs; then
+if ! yum list installed | grep nodejs; then
 	sudo yum install -y gcc-c++ make
 	curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
 	sudo yum install -y nodejs
